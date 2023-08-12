7ebbf10c43c9d0e3f6b5d4afb8019c19d71b2440
Janrupf <business.janrupf@gmail.com>
2023-08-13 01:50:30 +0200

    Attempt to auto detect protoc-gen-dart location

--- app/gen-protobufs.sh
+++ app/gen-protobufs.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-gen_plugin="$(realpath ~/.pub-cache/bin/protoc-gen-dart)"
+gen_plugin=$(command -v protoc-gen-dart)
+if [[ -z "$gen_plugin" ]]; then
+  gen_plugin="$(realpath ~/.pub-cache/bin/protoc-gen-dart)"
+fi
+
+echo "Using protoc-gen-dart at $gen_plugin"
+
 exec protoc \
   --dart_out=grpc:lib/generated \
   --plugin="$gen_plugin" \
